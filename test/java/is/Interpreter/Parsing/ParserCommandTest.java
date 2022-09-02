d9bf718095a63331af8f0bdf46e66e7be716503c
AlienSK1 <tempofabrizio@gmail.com>
2022-09-02 22:45:29 +0200

    Updated ParserCommandTest.

--- test/java/is/Interpreter/Parsing/ParserCommandTest.java
+++ test/java/is/Interpreter/Parsing/ParserCommandTest.java
@@ -13,43 +13,42 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 
 class ParserCommandTest {
-    static List<String> correctCommand;
-    static List<String> wrongCommand;
+    static List<String> commands;
     @BeforeAll
     public static void init(){
-        correctCommand= new LinkedList<>();
-        correctCommand.add("new circle (29) (299,299)");
-        correctCommand.add("new img (\"/home/fabrizio/Scaricati/Save_webP/pippo.png\") (200,200)");
-        correctCommand.add("mv 0 (100,100)");
-        correctCommand.add("mvoff 1 (100,100)");
-        correctCommand.add("grp 0,1");
-        correctCommand.add("ungrp 2");
-        correctCommand.add("ls all");
-        correctCommand.add("perimeter circle");
-        correctCommand.add("area all");
-        correctCommand.add("scale 1 2.5");
-        correctCommand.add("del 0");
-        wrongCommand= new LinkedList<>();
-        wrongCommand.add("(.../");
-        wrongCommand.add("new circle (100) (29,29) mv 0");
-        wrongCommand.add("new square (100) (20,20)");
-        wrongCommand.add("mv 0 (200,200)");
-        wrongCommand.add("");
-        wrongCommand.add("list circle");
-        wrongCommand.add("del 0");
+        commands= new LinkedList<>();
+        commands.add("new circle (29) (299,299)");
+        commands.add("new img (\"/home/fabrizio/Scaricati/Save_webP/pippo.png\") (200,200)");
+        commands.add("mv 0 (100,100)");
+        commands.add("mvoff 1 (100,100)");
+        commands.add("grp 0,1");
+        commands.add("ungrp 2");
+        commands.add("ls all");
+        commands.add("perimeter circle");
+        commands.add("area all");
+        commands.add("scale 1 2.5");
+        commands.add("del 0");
+        commands.add("(.../");
+        commands.add("new circle (100) (29,29) mv 0");
+        commands.add("new square (100) (20,20)");
+        commands.add("mv 0 (200,200)");
+        commands.add("");
+        commands.add("list circle");
+        commands.add("del 0");
     }
 
-    @RepeatedTest(11)
+    @RepeatedTest(17)
     public void CommandExecutionWorks(RepetitionInfo info){
-        String command= correctCommand.get(info.getCurrentRepetition()-1);
-        ParserCommand parser = new ParserCommand(new StringReader(command));
-        assertDoesNotThrow(()->parser.getCommand().interpret());
-    }
 
-    @RepeatedTest(6)
-    public void CommandExecutionFail(RepetitionInfo info){
-        String command = wrongCommand.get(info.getCurrentRepetition()-1);
-        assertThrows("Operazione fallita.",Exception.class,()-> new ParserCommand(new StringReader(command)).getCommand().interpret());
+        String command= commands.get(info.getCurrentRepetition()-1);
+        if(info.getCurrentRepetition()<12){
+            ParserCommand parser = new ParserCommand(new StringReader(command));
+            assertDoesNotThrow(()->parser.getCommand().interpret());
+        }
+        else{
+
+            assertThrows("Operazione fallita.",Exception.class,()-> new ParserCommand(new StringReader(command)).getCommand().interpret());
+        }
     }
 
 }
